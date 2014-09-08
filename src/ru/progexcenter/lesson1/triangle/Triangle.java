c8d8ad21b07dbe3bdee532d5fcb956079e3d7d6c
vlad <vlastachu@gmail.com>
2014-09-08 22:46:51 +0400

    Update Triangle.java

--- src/ru/progexcenter/lesson1/triangle/Triangle.java
+++ src/ru/progexcenter/lesson1/triangle/Triangle.java
@@ -9,8 +9,8 @@ class Triangle {
      * Entry point of application.
      * @argument array of strings which represent command line arguments
      */
-    public static void main (String args []) {
-        int edgeSize = Integer.parseInt(args[1]);
+    public static void main (String[] args) {
+        int edgeSize = Integer.parseInt(args[0]);
         //TODO implement drawing
         
     } 
