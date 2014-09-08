8a5df65c1d3acbc32227dacc2f064820e0607bba
vlad <vlastachu@gmail.com>
2014-09-08 22:41:50 +0400

    Update CubicSolve.java

--- src/ru/progexcenter/lesson1/cubicsolve/CubicSolve.java
+++ src/ru/progexcenter/lesson1/cubicsolve/CubicSolve.java
@@ -10,10 +10,10 @@ class CubicSolve {
      * @argument array of strings which represent command line arguments
      */
     public static void main (String args []) {
-        float a = Float.parseFloat(args[1]);
-        float b = Float.parseFloat(args[2]);
-        float c = Float.parseFloat(args[3]);
-        float d = Float.parseFloat(args[4]);
+        float a = Float.parseFloat(args[0]);
+        float b = Float.parseFloat(args[1]);
+        float c = Float.parseFloat(args[2]);
+        float d = Float.parseFloat(args[3]);
         //TODO implement calculation and output
         
     } 
